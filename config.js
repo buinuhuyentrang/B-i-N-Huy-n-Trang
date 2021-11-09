299a25a8a5f7da07706ee4d0a97c4d5999304273
buinuhuyentrang <93972891+buinuhuyentrang@users.noreply.github.com>
2021-11-09 16:01:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo !Bùi Nữ Huyền Chang nè 🖐🖐🖐',
-    introDesc: 'Trang có điều này muốn hỏi you, you nhớ phải trả lời thật lòng nha',
+    introTitle: '🖐🖐🖐 Hee loo !Nguyễn Thị Lê Na nè 🖐🖐🖐',
+    introDesc: 'Na đen có điều này muốn hỏi you, you nhớ phải trả lời thật lòng nha',
     btnIntro: 'OK',
-    title: 'Trang biết you thích Trang phải không phải không ? 😙',
-    desc: 'You không trả lời hoặc thoát ra, tức là You thích Trang đó nha kkk:> ',
-    btnYes: 'Có, tao thật sự thích Trang 💖💖💖',
-    btnNo: '💔 Trang mơ à ! 💔 ',
-    question:'Nói cho Trang biết đi vì sao you thích Trang 😊😊',
+    title: 'Na đen biết you thích Na phải không phải không ? 😙',
+    desc: 'You không trả lời hoặc thoát ra, tức là You thích Na đen đó nha kkk:> ',
+    btnYes: 'Có, tao thật sự thích Na 💖💖💖',
+    btnNo: 'Na đen mơ à ! 💔 ',
+    question:'Nói cho Na đen biết đi vì sao you thích Na đen 😊😊',
     reasonPlaceholder: 'Whyyyy',
     btnReply: 'Gửi ! 💌💌',
     reply: 'Yêu là yêu không yêu thì yêu Ahiiiii 😊😊',
-    mess: 'Trang biết mà, Trang cũng thích you lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Ib cho Trang liền kkk. 💬💬',
+    mess: 'Na đen biết mà, Na đen cũng thích you lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Ib cho Na đen liền kkk. 💬💬',
     btnAccept: 'Ok lun 💖',
-    messLink: 'https://www.facebook.com/huyen.tranqq.75'
+    messLink: 'https://www.facebook.com/nguyenthilena0305'
 }
